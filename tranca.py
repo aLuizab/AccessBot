eb986e401460d191f5008b0312cc97961c29e84e
Ana Luiza Botelho <aluiza.primo@gmail.com>
2018-02-18 00:03:57 -0300

    Tranca bot first commit

---
+++ tranca.py
@@ -0,0 +1,30 @@
+﻿#!/usr/bin/python
+import telepot, time
+
+
+def handle(msg):
+
+	list userName = ['ana','anne','derick']
+
+	content_type, chat_type, chat_id = telepot.glance(msg)
+
+	if (content_type == 'text'):
+		command = msg['text']
+		print ('Got command: %s' % command)
+
+	if userName in command:
+		bot.sendMessage(chat_id, "Ola "+userName+ ", Seja bem vindo a nossa nova sede, para entrar conecte-se à rede wifi da Acception.")
+	
+
+	if '/open' in command:
+		bot.sendMessage(chat_id, "Tranca aberta, "+userName+" entrando.")
+
+
+	
+bot = telepot.Bot('token')
+
+# Adiciona a função hanldle para ser chamada sempre que uma nova mensagem for recebida
+bot.message_loop(handle)
+
+while 1:
+	time.sleep(20)
